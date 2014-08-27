473f60dcb37f15609d848d1be878d09cfff919ea
hcadavid <hcadavid@10.1.15.28>
2014-08-27 12:07:42 -0500

    Prueba de conexión con la configuración de hibernate

---
+++ src/test/java/edu/eci/cosw/test/ConnectionTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.eci.cosw.test;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class ConnectionTest {
+    
+    @Test
+    public void connectionTest(){
+        SessionFactory sf=null;
+        try{
+            Configuration configuration = new Configuration();
+            configuration.configure("hibernate.cfg.xml");
+            ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+            sf = configuration.buildSessionFactory(serviceRegistry);                    
+            sf.openSession();
+        }
+        catch (Throwable e){
+            fail("Error en la configuración de la conexión:"+e.getLocalizedMessage());
+        }
+        finally{
+            if (sf!=null){
+                sf.close();
+            }
+        }
+    }
+    
+}
